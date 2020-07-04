fbae62e6ba36902770f1cb786fecce1f5ea4ca3e
ismail <bulbull.ismail@gmail.com>
2020-07-04 15:51:23 +0300

    add readFromFile

--- main.go
+++ main.go
@@ -1,12 +1,6 @@
 package main
 
 func main() {
-
-	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 5)
-
-	hand.print()
-	remainingCards.print()
-	hand.saveToFile("my__last_hand")
+	cards := newDeckFromFile("my_last_hand")
+	cards.print()
 }
